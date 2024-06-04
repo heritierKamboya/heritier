f38cd2c2d2c046d4c79ac952685d7f96a815f19e
Kamboya <kamboyah@gmail.com>
2024-06-03 18:59:15 -0700

    mon premier tp

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react/jsx-runtime"
+const title = 'Mon titre';
+function App() {
+ return <Fragment><h1 className='title'>{title}</h1>
+ <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates sapiente, modi ab itaque et atque provident voluptas molestiae totam omnis nisi quos sint quibusdam 
+      ipsum error consectetur rem ratione necessitatibus.
+ </p>
+ </Fragment>
+}
+
+export default App
